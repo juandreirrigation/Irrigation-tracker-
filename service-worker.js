66400ecdfccd71cefad1eab41759c47fde4eeff5
juandreirrigation <juandreh81@gmail.com>
2025-06-18 14:23:08 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('irrigation-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/service-worker.js',
+        '/icon-192.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
